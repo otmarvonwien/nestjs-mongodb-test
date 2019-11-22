ceb8962a0635ef3f3b72ef2e7ed68ff596879f14
Otmar Braunecker <otmarvonwien@hotmail.com>
2019-11-22 17:35:27 +0100

    mongo db connection setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const mongoDbOptions = {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-test', mongoDbOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
